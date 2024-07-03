17a47a60bd2bc5fc3556f591f7f2238517aed950
IslamGoher <islam.m.goher@gmail.com>
2024-07-04 00:02:32 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+// import "fmt"
+
+func main() {
+	/* const (
+		PI = 3.14
+		C  = 38
+		F  = ((9 / 5) * (C - 32))
+	)
+	println(PI)
+	var name string = "islam"
+	var name2 any
+	if 1 < 2 {
+		name2 = "inside the if statement"
+	} else {
+		name2 = "inside the else statement"
+	}
+	fmt.Println(name2)
+	fmt.Printf("hello %v how are %v\n", "world", name) */
+	var result int16 = -128
+	var y float64 = 1.7976931e+308
+	println(result)
+	println(y)
+	println(255 - 128)
+}
